c8af29870bbb5abca5753e4ef7b2266dd6d278ab
ruuuu <rufinka_91@mail.ru>
2026-03-23 19:13:21 +0300

    add two components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+
+
+
+
+export const Header = () => {
+
+  return (
+    <header className="header">
+        <div className="header-container">
+            <a href="/">ToDo</a>
+        </div>
+    </header> 
+  )
+} 
--- src/pages/ToDoList.tsx
+++ src/pages/ToDoList.tsx
@@ -1,3 +1,6 @@
+import { Form } from "../components/Form/Form"
+import { Header } from "../components/Header/Header"
+
 
 
 
@@ -5,6 +8,32 @@
 export const ToDoList = () => {
 
   return (
-    <span> ToDoList Page </span>
+    <> 
+   
+    <Header />
+
+    <Form />
+    <div className="todo-container">
+        <ul className="todo-list failed">
+            <li className="todo-list-item__wrapper">
+                <span>Первая задача</span>
+                <div className="todo-list-item__buttons">
+                    <button className="btn-trash"></button>
+                    <button className="btn-check"></button>
+                </div>
+            </li>
+        </ul>
+        <ul className="todo-list completed">
+            <li className="todo-list-item__wrapper">
+                <span>Вторая задача</span>
+                <div className="todo-list-item__buttons">
+                    <button className="btn-trash"></button>
+                    <button className="btn-uncheck"></button>
+                </div>
+            </li>
+        </ul>
+    </div>
+    </>
+    
   )
 }
