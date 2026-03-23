35ae02cddc7eb63785eb57ec5e565906911e2e6d
ruuuu <rufinka_91@mail.ru>
2026-03-23 19:40:40 +0300

    add styles for components

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,3 +1,4 @@
+import './Form.scss'
 
 
 export const Form = () => {
--- src/components/ToDoList/ToDoListItem/ToDoListItem.tsx
+++ src/components/ToDoList/ToDoListItem/ToDoListItem.tsx
@@ -1,6 +1,16 @@
+import './ToDoListItem.scss'
+
 
 export const ToDoListItem = () => {
 
-  
+  return (
+    <li className="todo-list-item__wrapper">
+      <span>Первая задача</span>
+      <div className="todo-list-item__buttons">
+        <button className="btn-trash"></button>
+        <button className="btn-check"></button>
+      </div>
+    </li>
+  )
 
 }
